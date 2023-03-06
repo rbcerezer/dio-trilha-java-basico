7e8a7f43df958fd5916cb17f119cbbeafba6976d
rbcerezer <rb.cerezer@gmail.com>
2023-03-06 22:34:50 +0100

    declarando algumas variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14
+
+        String meuNome = "Rafael Benetti"
+
+
     }
 }
