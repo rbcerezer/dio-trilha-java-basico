7951d7386b90964682afc65be53e1ab8d53bde90
rbcerezer <rb.cerezer@gmail.com>
2023-03-07 22:53:55 +0100

    Aprendendo sobre comentarios e javadoc

---
+++ JavaDoc/src/Comentarios1.java
@@ -0,0 +1,27 @@
+public class Comentarios1 {
+    public static void main(String[] args){
+        //testando o comentário one line
+
+        /*Comentário um pouco
+         * mais robusto, onde eu possa precisar de 
+         * maior de talhes ou explicações técnicas
+         */
+
+
+         
+    }
+
+    /**
+          * O sinal duplo de x (multiplicação),
+          nos mostra que este texto é elaborado 
+          a nível de documentação
+          */
+
+                   
+          public void metodo (){
+            
+          }
+
+          //javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+          // codigo para digitar no terminal criação de documentação
+}
